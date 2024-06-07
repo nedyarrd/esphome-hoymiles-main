14046c5c2b926f7b49c50706cf55b918b95988c4
Nedyarrd <nedyarrd@wp.pl>
2024-06-08 00:49:45 +0200

    Separate Hoymiles lib

---
+++ src/inverters/HMS_Abstract.cpp
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2023-2024 Thomas Basler and others
+ */
+#include "HMS_Abstract.h"
+#include "Hoymiles.h"
+#include "HoymilesRadio_CMT.h"
+#include "commands/ChannelChangeCommand.h"
+
+HMS_Abstract::HMS_Abstract(HoymilesRadio* radio, const uint64_t serial)
+    : HM_Abstract(radio, serial)
+{
+}
+
+bool HMS_Abstract::sendChangeChannelRequest()
+{
+    if (!(getEnableCommands() || getEnablePolling())) {
+        return false;
+    }
+
+    auto cmdChannel = _radio->prepareCommand<ChannelChangeCommand>();
+    cmdChannel->setCountryMode(Hoymiles.getRadioCmt()->getCountryMode());
+    cmdChannel->setChannel(Hoymiles.getRadioCmt()->getChannelFromFrequency(Hoymiles.getRadioCmt()->getInverterTargetFrequency()));
+    cmdChannel->setTargetAddress(serial());
+    _radio->enqueCommand(cmdChannel);
+
+    return true;
+};
---
+++ src/inverters/HMT_Abstract.cpp
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2023-2024 Thomas Basler and others
+ */
+#include "HMT_Abstract.h"
+#include "Hoymiles.h"
+#include "HoymilesRadio_CMT.h"
+#include "commands/ChannelChangeCommand.h"
+#include "parser/AlarmLogParser.h"
+
+HMT_Abstract::HMT_Abstract(HoymilesRadio* radio, const uint64_t serial)
+    : HM_Abstract(radio, serial)
+{
+    EventLog()->setMessageType(AlarmMessageType_t::HMT);
+};
+
+bool HMT_Abstract::sendChangeChannelRequest()
+{
+    if (!(getEnableCommands() || getEnablePolling())) {
+        return false;
+    }
+
+    auto cmdChannel = _radio->prepareCommand<ChannelChangeCommand>();
+    cmdChannel->setCountryMode(Hoymiles.getRadioCmt()->getCountryMode());
+    cmdChannel->setChannel(Hoymiles.getRadioCmt()->getChannelFromFrequency(Hoymiles.getRadioCmt()->getInverterTargetFrequency()));
+    cmdChannel->setTargetAddress(serial());
+    _radio->enqueCommand(cmdChannel);
+
+    return true;
+};
